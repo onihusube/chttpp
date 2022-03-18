32dfaf361a777a997e8b8dad22f66b72218a6268
onihusube <44743040+onihusube@users.noreply.github.com>
2022-03-18 18:08:56 +0900

    テストちょと追加（無効化してる

--- test/http_result_test.hpp
+++ test/http_result_test.hpp
@@ -14,6 +14,14 @@ auto hr_err() -> chttpp::http_result {
 
   return chttpp::http_result{E{}};
 }
+/*
+auto hr_exptr() -> chttpp::http_result {
+  try {
+    throw std::runtime_error{"exception test."};
+  } catch (...) {
+    return chttpp::http_result{std::current_exception()};
+  }
+}*/
 
 void http_result_test() {
   using namespace boost::ut::literals;
@@ -32,8 +40,44 @@ void http_result_test() {
       }).then([](http_response&& hr) {
         ut::expect(hr.status_code == 200_i);
         return hr;
-      }).catch_err([](const auto&) {
+      }).catch_error([](const auto&) {
+        ut::expect(false);
+      }).catch_exception([](const auto&) {
         ut::expect(false);
       });
   };
+
+/*
+  "catch_exception"_test = []
+  {
+    hr_exptr().then([](http_response&& hr) {
+        ut::expect(false);
+        return hr;
+      }).then([](http_response&& hr) {
+        ut::expect(false);
+        return hr;
+      }).then([](http_response&& hr) {
+        ut::expect(false);
+        return hr;
+      }).catch_error([](const auto&) {
+        ut::expect(false);
+      }).catch_exception([](const auto& exptr) {
+        try {
+          std::rethrow_exception(exptr);
+        } catch (const std::runtime_error& re) {
+          ut::expect(re.what() == "exception test."sv);
+        } catch (...) {
+          ut::expect(false);
+        }
+      }).catch_exception([](const auto& exptr) {
+        try {
+          std::rethrow_exception(exptr);
+        } catch (const std::runtime_error& re) {
+          ut::expect(re.what() == "exception test."sv);
+        } catch (...) {
+          ut::expect(false);
+        }
+      });
+  };
+  */
 }
